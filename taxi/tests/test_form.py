7a0b395b2e5bf4eac98ecc960507f338fea7b051
Valeriia Kyrylenko <valerya.kyrylenko@gmail.com>
2024-09-29 15:48:35 +0200

    implement taxi-service-search-and-test 4

--- taxi/tests/test_form.py
+++ taxi/tests/test_form.py
@@ -11,17 +11,48 @@ from taxi.models import Driver, Car, Manufacturer
 
 class FormsTest(TestCase):
     def setUp(self):
-        self.manufacturer1 = Manufacturer.objects.create(name="test", country="testcountry")
-        self.manufacturer2 = Manufacturer.objects.create(name="another", country="anothercountry")
+        self.manufacturer1 = (
+            Manufacturer.objects.create(
+                name="test",
+                country="testcountry"
+            )
+        )
+        self.manufacturer2 = (
+            Manufacturer.objects.create(
+                name="another",
+                country="anothercountry"
+            )
+        )
 
-        self.car1 = Car.objects.create(model="test", manufacturer=self.manufacturer1)
-        self.car2 = Car.objects.create(model="another", manufacturer=self.manufacturer2)
+        self.car1 = (
+            Car.objects.create(
+                model="test",
+                manufacturer=self.manufacturer1
+            )
+        )
+        self.car2 = (
+            Car.objects.create(
+                model="another",
+                manufacturer=self.manufacturer2
+            )
+        )
 
-        self.driver1 = Driver.objects.create_user(username="testuser", password="pass12345", license_number="ABC12345")
-        self.driver2 = Driver.objects.create_user(username="another", password="pass12345", license_number="XYZ56789")
+        self.driver1 = (
+            Driver.objects.create_user(
+                username="testuser",
+                password="pass12345",
+                license_number="ABC12345"
+            )
+        )
+        self.driver2 = (
+            Driver.objects.create_user(
+                username="another",
+                password="pass12345",
+                license_number="XYZ56789"
+            )
+        )
 
-
-    def test_driver_creation_form_with_license_number_name_is_valid(self):
+    def test_driver_creation_form_license_number_is_valid(self):
         form_data = {
             "username": "new_user",
             "password1": "user12test",
@@ -39,17 +70,24 @@ class FormsTest(TestCase):
         form = DriversSearchForm(data=form_data)
         self.assertTrue(form.is_valid())
         self.assertEqual(form.cleaned_data["username"], "testuser")
-        result = Driver.objects.filter(username__icontains=form.cleaned_data["username"])
+        result = (
+            Driver.objects.filter(
+                username__icontains=form.cleaned_data["username"]
+            )
+        )
         self.assertIn(self.driver1, result)
         self.assertNotIn(self.driver2, result)
-
 
     def test_car_search_form(self):
         form_data = {"model": "test"}
         form = CarsSearchForm(data=form_data)
         self.assertTrue(form.is_valid())
         self.assertEqual(form.cleaned_data["model"], "test")
-        results = Car.objects.filter(model__icontains=form.cleaned_data["model"])
+        results = (
+            Car.objects.filter(
+                model__icontains=form.cleaned_data["model"]
+            )
+        )
         self.assertIn(self.car1, results)
         self.assertNotIn(self.car2, results)
 
@@ -58,6 +96,10 @@ class FormsTest(TestCase):
         form = ManufacturerSearchForm(data=form_data)
         self.assertTrue(form.is_valid())
         self.assertEqual(form.cleaned_data["name"], "test")
-        results = Manufacturer.objects.filter(name__icontains=form.cleaned_data["name"])
+        results = (
+            Manufacturer.objects.filter(
+                name__icontains=form.cleaned_data["name"]
+            )
+        )
         self.assertIn(self.manufacturer1, results)
         self.assertNotIn(self.manufacturer2, results)
